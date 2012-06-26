6640c42d9574133c55f41e39c7f90de7b653fcc6
Ronald Valente <rawn027@gmail.com>
2012-06-26 09:06:03 -0400

    updated to use the top path of git dir

--- setupvim.sh
+++ setupvim.sh
@@ -19,9 +19,6 @@ git submodule update
 # Ensure there is a bundle directory for pathogen
 mkdir -p ~/.vim/bundle
 
-# Setup submodules
-cd ~/.vim/bundle
-
 # Create list of modules to add
 modules=( https://github.com/scrooloose/syntastic.git https://github.com/rodjek/vim-puppet.git https://github.com/altercation/vim-colors-solarized.git https://github.com/kien/ctrlp.vim.git https://github.com/godlygeek/tabular.git https://github.com/msanders/snipmate.vim.git https://github.com/tpope/vim-fugitive.git https://github.com/tpope/vim-pastie.git https://github.com/tpope/vim-markdown.git https://github.com/scrooloose/nerdtree.git )
 
