e84677b986051adac10cd92c458689eff6e39c47
Ronald Valente <rawn027@gmail.com>
2012-07-02 06:57:29 -0400

    updated pkgjs

--- pkgs.sh
+++ pkgs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-gems=[puppet,puppet-lint,methadone,pry]
-pkgs=[git,git-flow,wget,ack,bash-completion,tmux]
+gems=[puppet,puppet-lint,methadone,pry,tmuxinator,httparty]
+pkgs=[git,git-flow,wget,ack,bash-completion,tmux,fping]
 
 for i in gems
