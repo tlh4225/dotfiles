cd08272870dd485f5d98869db44fffca4ef70dce
Ronald Valente <rawn027@gmail.com>
2012-07-02 09:36:02 -0400

    updated packages file

--- pkgs.sh
+++ pkgs.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
-gems=[puppet,puppet-lint,methadone,pry,tmuxinator,httparty]
-pkgs=[git,git-flow,wget,ack,bash-completion,tmux,fping]
+gems=(puppet puppet-lint methadone pry tmuxinator httparty)
+pkgs=(git git-flow wget ack bash-completion tmux fping)
 
-for i in gems
+for gem in ${gems[*]}; do
+  if [[ -f `which rvm` ]]; then
+    echo "Install Gem: $gem..."
+    gem install $gem
+  else
+    echo "Install RVM"; break
+  fi
+done
+
+for pkg in ${pkgs[*]}; do
+  if [[ -f `which brew` ]]; then
+    echo "Installing Package: $pkg..."
+    brew install $pkg
+  else
+    echo "Install Homebrew"; break
+  fi
+done
