32cc5fce56243e6674dc9af99bf1a709dd1a6c4f
Ronald Valente <rawn027@gmail.com>
2013-02-07 08:57:03 -0500

    updated

---
+++ README.md
@@ -0,0 +1,16 @@
+# Installation
+
+First: Clone repo anywhere you want, remember the path.
+
+```
+cd ~/Development
+git clone git://github.com/rvalente/dotfiles.git
+```
+
+Now we want to make symlinks of all the files in your home directory.
+
+```
+cd ~/Development/dotfiles
+bash setupfiles.sh
+```
+
--- setupfiles.sh
+++ setupfiles.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-files=( gitconfig gitignore irbrc rvmrc pryrc gemrc bash_aliases bash_profile bashrc bash_login bash_colors vimrc taskrc tmux.conf ctags )
+files=( gitconfig gitignore irbrc rvmrc pryrc gemrc bash_aliases bash_profile bashrc bash_colors vimrc taskrc ctags )
 
 for element in $(seq 0 $((${#files[@]} - 1))); do
   file="${files[$element]}"
   dotfile=".${file}"
   pwd=`pwd`
 
+  mv ~/"${dotfile}" ~/"${dotfile}".bak
   ln -sf "${pwd}/${file}" ~/"${dotfile}"
 done
 
